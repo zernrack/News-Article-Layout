0a5732966820f5d6f032782e7642324c44395deb
Renz <rackhold@gmail.com>
2023-03-23 18:50:14 +0800

    Add material icon on packages

---
+++ client/src/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ArticlePage() {
+  return (
+    <>
+    <div className='flex justify-center items-center'>
+        <h1>Hello world</h1>
+    </div>
+    </>
+  )
+}
+
+export default ArticlePage
